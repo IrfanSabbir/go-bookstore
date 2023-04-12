6c141f7149661ff78c94e738ca35ef83c69e16c9
IrfanSabbir <avoidirfan@gmail.com>
2023-04-13 00:55:47 +0600

    Login functionality updated with auth token

--- go.mod
+++ go.mod
@@ -3,13 +3,15 @@ module github.com/IrfanSabbir/go-bookstore
 go 1.18
 
 require (
+	github.com/golang-jwt/jwt v3.2.2+incompatible
 	github.com/gorilla/mux v1.8.0
 	github.com/jinzhu/gorm v1.9.16
+	github.com/joho/godotenv v1.5.1
+	golang.org/x/crypto v0.7.0
 )
 
 require (
 	github.com/go-sql-driver/mysql v1.7.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
-	golang.org/x/crypto v0.7.0 // indirect
 )
--- pkg/controllers/auth-controller.go
+++ pkg/controllers/auth-controller.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net/http"
 
+	jwt_token "github.com/IrfanSabbir/go-bookstore/pkg/jwt"
 	model "github.com/IrfanSabbir/go-bookstore/pkg/models"
 	"github.com/IrfanSabbir/go-bookstore/pkg/utils"
 )
@@ -12,6 +13,11 @@ import (
 type AuthBody struct {
 	email    string
 	password string
+}
+type AuthUserResponse struct {
+	User    map[string]interface{} `json:"user"`
+	Message string                 `json:"message"`
+	Token   string                 `json:"token"`
 }
 
 func RegisterUser(w http.ResponseWriter, r *http.Request) {
@@ -29,9 +35,29 @@ func Login(w http.ResponseWriter, r *http.Request) {
 	authBody := model.User{}
 	utils.ParseBody(r, &authBody)
 	authUser := model.Login(authBody.Email, authBody.Password)
-	fmt.Println(authUser)
-	res, _ := json.Marshal(authUser)
+	fmt.Println("authUser", authUser)
+	tokenString, err := jwt_token.GenerateToken(int64(authUser.ID))
+	fmt.Println("tokenString", tokenString)
 
+	if err != nil {
+		fmt.Println(err)
+	}
+	respObject := AuthUserResponse{
+		User: map[string]interface{}{
+			"id":    authUser.ID,
+			"name":  authUser.Name,
+			"email": authUser.Email,
+			"role":  authUser.Role,
+		},
+		Message: "Successfullt login",
+		Token:   tokenString,
+	}
+
+	fmt.Println(respObject)
+	res, err := json.Marshal(respObject)
+	if err != nil {
+		fmt.Println(err)
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
